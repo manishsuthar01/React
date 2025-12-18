1e1f1704ad1c761391d7a9f6ff7d83789918b8bc
Manish suthar <manishsuthar78775@gmail.com>
2025-12-18 18:52:15 +0530

    Delete carpenter_port directory

--- carpenter_port/src/Pages/allProject.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const allProject = () => {
-  return (
-    <div>allProject</div>
-  )
-}
-
-export default allProject
--- carpenter_port/src/data/portfolioData.js
+++
@@ -1,6 +0,0 @@
-export const portfolioItems = [
-  {Image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu006-MSam-lYoPcYB3Kj7ODubQT-IzNiKoQ&s",title:"Elegant Oak Dining Table",description:"A handcrafted centerpiece made from premium oak, featuring a smooth matte finish and timeless design to complement modern and classic interiors."},
-  {Image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0v1JBbj2IUsP3rEMR4oObQU74wgLyaYph3w&s",title:"Bespoke Lounge Chair",description:"Expertly upholstered and ergonomically designed, this custom chair blends comfort with refined craftsmanship for a truly unique seating experience."},
-  {Image:"https://www.bauformatbc.com/wp-content/uploads/German-custom-cabinets-Bauformat-Series-18-scaled.jpg",title:"Contemporary Kitchen Cabinetry",description:"Sleek, functional, and built to last — our custom kitchen cabinets combine innovative storage solutions with the warmth of fine woodwork."}
-];
-  
